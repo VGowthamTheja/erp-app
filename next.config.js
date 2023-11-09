d5a1a8cdf1f2822f733a66a640547f8f566a6937
VGowthamTheja <vempalligowthamtheja@gmail.com>
2023-11-09 23:21:06 +0530

    Initialized Docker container and started prisma ORM

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
